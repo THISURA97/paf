619efc9716d67e3da2f0e48a7a07fe76e368bb54
THISURA97 <38825184+THISURA97@users.noreply.github.com>
2020-04-15 23:51:06 +0530

    workflow modify 1.3

--- ScheduleService/com/ScheduleService.java
+++ ScheduleService/com/ScheduleService.java
@@ -12,10 +12,8 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-
 import beans.ScheduleBean;
 import model.Schedule;
 
