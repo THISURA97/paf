47ba97d0dce5d5a6468ac4f2640d4ed7be3272bd
Himashinee1996 <himashineethareendra0502@gmail.com>
2020-04-15 19:19:36 +0530

    first commit

---
+++ HealthCare1/src/demo/UserDemo.java
@@ -0,0 +1,144 @@
+package demo;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import model.User;
+
+@XmlRootElement
+public class UserDemo {
+	
+	int id;
+	private String firstName;
+	private String lastName;
+	private String dob;
+	private String age;
+	private String gender;
+	private String address;
+	private String phone;
+	private String email;
+	
+	
+	public UserDemo() {}
+	
+	public UserDemo(String usr) {
+		
+		JsonObject userObject = new JsonParser().parse(usr).getAsJsonObject();
+		
+		if (userObject.get("userID") !=null) {
+			
+			this.id = userObject.get("userID").getAsInt();
+		}
+		
+		this.firstName = userObject.get("firstName").getAsString();	
+		this.lastName = userObject.get("lastName").getAsString();	
+		this.dob = userObject.get("dob").getAsString();	
+		this.age = userObject.get("age").getAsString();	
+		this.gender = userObject.get("gender").getAsString();	
+		this.address = userObject.get("address").getAsString();	
+		this.phone = userObject.get("phone").getAsString();
+		this.email = userObject.get("email").getAsString();	
+	}
+	
+	public UserDemo (int id, String firstName, String lastName, String dob, String age, String gender, String address, String phone, String email) {
+		
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.age = age;
+		this.gender = gender;
+		this.address = address;
+		this.phone = phone;
+		this.email = email;
+	}
+	
+	public UserDemo (String firstName, String lastName, String dob, String age, String gender, String address, String phone, String email) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.age = age;
+		this.gender = gender;
+		this.address = address;
+		this.phone = phone;
+		this.email = email;
+	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstName;
+	}
+
+	public void setFirstname(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLaststname() {
+		return lastName;
+	}
+
+	public void setLaststname(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
+	
+	
+	
+}
---
+++ HealthCare1/src/model/User.java
@@ -0,0 +1,222 @@
+package model;
+
+import demo.UserDemo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+	
+	private Connection connect() {
+		
+		Connection con = null;
+		
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			//DB name, username, password
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3308/healthcare", "root", "");
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+	
+	public String insertUser(UserDemo usr) {
+		
+		String output = "";
+		
+		try {
+			
+			Connection con = connect();
+			
+			if (con == null) {
+				
+				return "Database connection error while inserting details";
+			}
+			
+			// prepared statements
+			String userQuery = "inser into user" + "(userID, firstName, lastName, dob, age, gender, address, phone, email)" + "values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			
+			PreparedStatement preparedStmtUser = con.prepareStatement(userQuery);
+			
+			//Binding values for user Table
+			preparedStmtUser.setInt(1, 0);
+			preparedStmtUser.setString(2, usr.getFirstname());
+			preparedStmtUser.setString(3, usr.getLaststname());
+			preparedStmtUser.setString(4, usr.getDob());
+			preparedStmtUser.setInt(5, Integer.parseInt(usr.getAge()));
+			preparedStmtUser.setString(6, usr.getGender());
+			preparedStmtUser.setString(7, usr.getAddress());
+			preparedStmtUser.setString(8, usr.getPhone());
+			preparedStmtUser.setString(9, usr.getEmail());
+			
+			//Execute the statement
+			preparedStmtUser.execute();
+			
+			con.close();
+			
+			output = "Inserted successfully.";
+			
+		} catch (Exception e) {
+			
+			output = "Error while inserting details";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+	
+ 	public List<UserDemo> readUser(){
+ 		
+ 		List<UserDemo> usrList = new ArrayList<>();
+ 		
+ 		String output = "";
+ 		
+ 		try {
+ 			
+ 			Connection con = connect();
+ 			
+ 			if (con == null) {
+ 				
+ 				System.out.println("Database Connection error while reading details");
+ 				return usrList;
+ 			}
+ 			
+ 			String query = "select u.userID, u.firstName, u.lastNmae, u.dob, u.age, u.gender, u.address, u.phone, u.email from user";
+ 			
+ 			Statement stmt = con.createStatement();
+ 			ResultSet rslt = stmt.executeQuery(query);
+ 			
+ 			while (rslt.next()) {
+ 				
+ 				UserDemo usr = new UserDemo(
+ 						
+ 						rslt.getInt("userID"),
+ 						rslt.getString("firstName"),
+ 						rslt.getString("laststName"),
+ 						rslt.getString("dob"),
+ 						rslt.getString("age"),
+ 						rslt.getString("gender"),
+ 						rslt.getString("address"),
+ 						rslt.getString("phone"),
+ 						rslt.getString("email")
+ 						
+ 				);
+ 				
+ 				usrList.add(usr);
+ 				
+ 			}
+ 			
+ 			con.close();
+
+ 		} catch (Exception e) {
+ 			
+ 			System.out.println("Error while reading details");
+ 			System.err.println(e.getMessage());
+ 		}
+ 		
+ 		return usrList;
+ 		
+ 	}
+ 	
+ 	public String updateUser(UserDemo usr) {
+ 		
+ 		String output = "";
+ 		
+ 		try {
+ 			
+ 			Connection con = connect();
+ 			
+ 			if (con == null) {
+ 				
+ 				return "Database connection error while updating the user details.";
+ 				
+ 			}
+ 			
+ 			//prepared statements
+ 			
+ 			String userQuery = "UPDATE user SET" + "firstName=?," + "lastName=?," + "dob=?," + "age=?," + "gender=?," + "phone=?," + "email=?" + "WHERE userID=?";
+ 			
+ 			PreparedStatement userDetails = con.prepareStatement(userQuery);
+ 			
+ 			//Binding values for userQuery
+ 			userDetails.setString(1, usr.getFirstname());
+ 			userDetails.setString(2, usr.getLaststname());
+ 			userDetails.setString(3, usr.getDob());
+ 			userDetails.setString(4, usr.getAge());
+ 			userDetails.setString(5, usr.getGender());
+ 			userDetails.setString(6, usr.getAddress());
+ 			userDetails.setString(7, usr.getPhone());
+ 			userDetails.setString(8, usr.getEmail());
+ 			userDetails.setInt(9, usr.getId());
+ 			
+ 			//Execute the statement
+ 			userDetails.execute();
+ 			
+ 			con.close();
+ 			
+ 			output = "User details updated successfully";
+ 		
+ 		} catch (Exception e) {
+ 			
+ 			output = "Error while updating details";
+ 			System.err.println(e.getMessage());
+ 		}
+ 		
+ 		return output;
+ 	
+ 	}
+ 	
+ 	public String deleteUser(String ID) {
+ 		
+ 		String output = "";
+ 		
+ 		try {
+ 			
+ 			Connection con = connect();
+ 			
+ 			if (con == null) {
+ 				
+ 				return "Database connection error while deleting details.";
+ 				
+ 			}
+ 			
+ 			// Prepared statements
+ 			String deleteUser = "delete from user where userID=?";
+ 			
+ 			PreparedStatement preparedStmtForUser = con.prepareStatement(deleteUser);
+ 			
+ 			//Binding values
+ 			preparedStmtForUser.setInt(1, Integer.parseInt(ID));
+ 			
+ 			//Executing statement
+ 			preparedStmtForUser.execute();
+ 			
+ 			con.close();
+ 			
+ 			output = "Deleted Successfully";
+ 		
+ 		} catch (Exception e) {
+ 			
+ 			output = "Error while deleting details";
+ 			System.err.println(e.getMessage());
+ 		}
+ 		
+ 		return output;
+ 	}
+ 	
+
+		
+}
+
+
